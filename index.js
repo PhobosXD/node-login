ab8e82b659ce988de40a4d220eab79e31cf5cad0
Igor Emerick <igorbarbosaemerick@gmail.com>
2021-03-15 15:52:56 -0300

    Autenticação de login

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv-safe').config();
+const jwt = require('jsonwebtoken');
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -20,6 +22,26 @@ app.get('/clientes', (req, res) => {
         console.log('[*]Retornou todos os clientes!');
     } catch (error) {
         console.log(error);
+    }
+});
+
+app.post('/login', (req, res) => {
+    //user = req.body.user;
+    //password = req.body.password;
+
+    if (req.body.user == 'cazuza' && req.body.password == 'teste') {
+        try {
+            const id = 1;
+            const token = jwt.sign({id}, process.env.SECRET, {expiresIn: 300});
+
+            console.log('[*]Login sucess!');
+            return res.json({auth: true, token: token});
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        console.log('[*]Login fail!');
+        res.status(500).json({message: 'Login inválido!'});
     }
 });
 
